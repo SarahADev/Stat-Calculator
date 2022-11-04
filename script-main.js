ff1cd5350b46f6663742ccc9a0f883e7d8638eef
Sarah Alexa <sarahalexa@Sarahs-MBP.lan>
2022-11-04 13:36:02 +0000

    Added clear functionality and input sanitising

---
+++ script-main.js
@@ -0,0 +1,49 @@
+// CLEAR
+const clear = document.querySelector(".clear");
+
+clear.addEventListener("click", (e) => {
+    clearInput();
+});
+
+function clearInput() {
+  let input = document.querySelector(".input");
+  input.value = "";
+  let error = document.querySelector(".error-message");
+  error.innerHTML = "";
+}
+
+// CALCULATE
+const calculate = document.querySelector(".calculate");
+
+calculate.addEventListener("click", (e) => {
+    calcSanitise();
+});
+
+function calcSanitise() {
+  let input = document.querySelector(".input");
+  if (!input.value){
+    // If no input
+    let error = document.querySelector(".error-message");
+    error.innerHTML = "Please input at least 2 values.";
+  } else {
+    // Removing invalid characters
+    let sanitisedInput = input.value.replace(/[^0-9\.\,]+/g, "");
+    input.value = sanitisedInput;
+    // Converting to array
+    let arr = sanitisedInput.split(",");
+    let intArr = [];
+    // Converting to number valid integers and floats
+    arr.forEach((number) => {
+      if (!isNaN(number * 1)) {
+          intArr.push(number*1);
+      }
+    });
+    input.value = intArr.join(',')
+    if (intArr.length < 2) {
+        let error = document.querySelector(".error-message");
+        error.innerHTML = "Please input at least valid 2 values.";
+    } else {
+        // Calc mean, calc median, calc mode
+    }
+  }
+}
