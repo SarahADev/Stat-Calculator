2e744058e1732bc887a40611af142d1acdd400e2
Sarah Alexa <sarahalexa@Sarahs-MBP.lan>
2022-11-04 13:38:55 +0000

    Added calcMean

--- script-main.js
+++ script-main.js
@@ -2,7 +2,7 @@
 const clear = document.querySelector(".clear");
 
 clear.addEventListener("click", (e) => {
-    clearInput();
+  clearInput();
 });
 
 function clearInput() {
@@ -16,12 +16,12 @@ function clearInput() {
 const calculate = document.querySelector(".calculate");
 
 calculate.addEventListener("click", (e) => {
-    calcSanitise();
+  calcSanitise();
 });
 
 function calcSanitise() {
   let input = document.querySelector(".input");
-  if (!input.value){
+  if (!input.value) {
     // If no input
     let error = document.querySelector(".error-message");
     error.innerHTML = "Please input at least 2 values.";
@@ -35,15 +35,28 @@ function calcSanitise() {
     // Converting to number valid integers and floats
     arr.forEach((number) => {
       if (!isNaN(number * 1)) {
-          intArr.push(number*1);
+        intArr.push(number * 1);
       }
     });
-    input.value = intArr.join(',')
+    input.value = intArr.join(",");
     if (intArr.length < 2) {
-        let error = document.querySelector(".error-message");
-        error.innerHTML = "Please input at least valid 2 values.";
+      let error = document.querySelector(".error-message");
+      error.innerHTML = "Please input at least valid 2 values.";
     } else {
-        // Calc mean, calc median, calc mode
+      //calc median, calc mode
+      document.querySelector(".value-mean").innerHTML = calcMean(intArr);
     }
   }
+}
+
+// MEAN
+
+function calcMean(values) {
+  let count = 0;
+  // Sum of values
+  values.forEach((number) => {
+    count = count + number;
+  });
+  // Sum divided by amount of values
+  return count / values.length;
 }
